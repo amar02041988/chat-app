92603f7ae5c6782e68dde65824bbff687ca181a2
U-EMEA\PanigraA <PanigraA@IN-MU-LT-27194.emea.travelex.net>
2019-03-11 17:52:10 +0530

    Ready to deploy

--- socket-server.js
+++ socket-server.js
@@ -8,27 +8,29 @@ var server = http.createServer(app);
 
 var io = require("socket.io")(server);
 
-io.on("connection", client =>{
+io.on("connection", client => {
     console.log("New client connected...", client.id);
-    client.emit("acknowledge", {data : "Connected"});
+    client.emit("acknowledge", { data: "Connected" });
 
     client.on("msgToServer", (chatterName, msg) => {
         console.log(chatterName + " says : " + msg);
-        client.broadcast.emit("msgToClient", chatterName , msg);
+        client.broadcast.emit("msgToClient", chatterName, msg);
         client.emit("msgToClient", 'Me', msg);
     })
 
-    client.on("disconnect", ()=>{
+    client.on("disconnect", () => {
         console.log("Client disconnected." + client.id);
         //Save data into DB
     })
 
 })
 
-app.get("/", (req, res)=>{
+
+app.get("/", (req, res) => {
     res.sendFile(__dirname + '/public/socket-client.html');
 })
 
-server.listen(3000, ()=>{
+var port = process.env.PORT || 3000;
+server.listen(port, () => {
     console.log("Socket server running on port 3000");
 })
